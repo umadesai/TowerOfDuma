84b4e93b32b025156f0b001cfdb1ba1df56f0676
umadesai <uma.desai@students.olin.edu>
2018-04-19 12:44:08 -0400

    Update game_state_start.hpp

--- include/game_state_start.hpp
+++ include/game_state_start.hpp
@@ -1,28 +1,11 @@
-<<<<<<< HEAD
-#ifndef GAME_STATE_START_HPP
-#define GAME_STATE_START_HPP
-=======
 #ifndef INCLUDE_GAME_STATE_START_HPP_
 #define INCLUDE_GAME_STATE_START_HPP_
->>>>>>> lint_errors
 
 #include <SFML/Graphics.hpp>
 
 #include "game_state.hpp"
 #include "game_state_editor.hpp"
 
-<<<<<<< HEAD
-class GameStateStart : public GameState
-{
-    private:
-
-    sf::View view;
-
-    void loadgame();
-
-    public:
-
-=======
 class GameStateStart : public GameState {
  private:
   sf::View view;
@@ -30,19 +13,11 @@ class GameStateStart : public GameState {
     void loadgame();
 
  public:
->>>>>>> lint_errors
     virtual void draw(const float dt);
     virtual void update(const float dt);
     virtual void handleInput();
 
-<<<<<<< HEAD
-    GameStateStart(Game* game);
-};
-
-#endif /* GAME_STATE_START_HPP */
-=======
     explicit GameStateStart(Game* game);
 };
 
 #endif  // INCLUDE_GAME_STATE_START_HPP_
->>>>>>> lint_errors
