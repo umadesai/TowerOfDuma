a1453dfc43b358d1a4567fd44730d3d7500a431f
Uma Desai <uma.desai@students.olin.edu>
2018-04-18 22:09:31 -0400

    update textures and animations

---
+++ include/game_state_start.hpp
@@ -0,0 +1,26 @@
+#ifndef GAME_STATE_START_HPP
+#define GAME_STATE_START_HPP
+
+#include <SFML/Graphics.hpp>
+
+#include "game_state.hpp"
+#include "game_state_editor.hpp"
+
+class GameStateStart : public GameState
+{
+    private:
+
+    sf::View view;
+
+    void loadgame();
+
+    public:
+
+    virtual void draw(const float dt);
+    virtual void update(const float dt);
+    virtual void handleInput();
+
+    GameStateStart(Game* game);
+};
+
+#endif /* GAME_STATE_START_HPP */
