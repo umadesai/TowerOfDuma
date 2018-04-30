992a8aa789be9b6d7c8389f2a88d8891c93d19b3
David Abrahams <david.abrahams@students.olin.edu>
2018-04-29 21:18:07 -0400

    add tower class

---
+++ src/tower.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include "../include/tower.hpp"
+
+Tower::Tower(int firerate, int damage, int range, int x, int y)
+      : firerate(firerate), damage(damage), range(range), x(x), y(y)
+{}
+
+
+Tower::~Tower()
+{}
