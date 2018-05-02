a8ff3f6e394342e26e74113d18fc0b0421ae1500
Uma Desai <uma.desai@students.olin.edu>
2018-05-01 21:52:26 -0400

    change pointers to vectors

--- include/map.hpp
+++ include/map.hpp
@@ -2,17 +2,18 @@
 #ifndef INCLUDE_MAP_HPP_
 #define INCLUDE_MAP_HPP_
 
+#include<vector>
 #include "../include/tower.hpp"
 #include "../include/enemy.hpp"
 #include "../include/waypoint.hpp"
 
 class Map {
  private:
-    Tower *towers;
-    Enemy *enemies;
+    std::vector<Tower> towers;
+    std::vector<Enemy> enemies;
     Waypoint *start;
  public:
-    Map(Tower towers[], Enemy enemies[], Waypoint *start);
+    Map(std::vector<Tower> towers, std::vector<Enemy> enemies, Waypoint *start);
     ~Map();
 };
 
