52c3f838962e6fb5d3f625512e48a38234713c88
David Abrahams <david.abrahams@students.olin.edu>
2018-05-01 21:14:51 -0400

    rename game state editor -> towers

---
+++ include/game_state_towers.hpp
@@ -0,0 +1,20 @@
+#ifndef INCLUDE_GAME_STATE_TOWERS_HPP_
+#define INCLUDE_GAME_STATE_TOWERS_HPP_
+
+#include <SFML/Graphics.hpp>
+
+#include "game_state.hpp"
+
+class GameStateTowers : public GameState {
+ private:
+  sf::View guiView;
+
+ public:
+  virtual void draw(const float dt);
+  virtual void update(const float dt);
+  virtual void handleInput();
+
+  explicit GameStateTowers(Game* game);
+};
+
+#endif  // INCLUDE_GAME_STATE_TOWERS_HPP_
